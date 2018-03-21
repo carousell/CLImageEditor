a8ec8a220454036ff2695342750283635ec0e81f
jianyaoang <jianyaoang1@gmail.com>
2018-03-21 14:40:55 +0800

    Update toolbar color

--- CLImageEditor/CLImageEditorTheme.m
+++ CLImageEditor/CLImageEditorTheme.m
@@ -42,9 +42,10 @@ static CLImageEditorTheme *_sharedInstance = nil;
 {
     self = [super init];
     if (self) {
+        CGFloat totalFloat = 255.0;
         self.bundleName                     = @"CLImageEditor";
-        self.backgroundColor                = [UIColor colorWithRed:75/255 green:77/255 blue:82/255 alpha:1];
-        self.toolbarColor                   = [UIColor colorWithWhite:1 alpha:0.8];
+        self.backgroundColor                = [UIColor colorWithRed:75/totalFloat green:77/totalFloat blue:82/totalFloat alpha:1];
+        self.toolbarColor                   = [UIColor colorWithRed:38/totalFloat green:38/totalFloat blue:41/totalFloat alpha:1];
 		self.toolIconColor                  = @"white";
         self.toolbarTextColor               = [UIColor whiteColor];
         self.toolbarSelectedButtonColor     = [[UIColor cyanColor] colorWithAlphaComponent:0.2];

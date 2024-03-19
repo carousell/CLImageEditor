8c469a6c0441db240c87b60b2bf5270132f57308
Vijay Tholpadi <vijay@thegeekprojekt.com>
2024-03-19 16:59:26 +0800

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CLImageEditor",
+    platforms: [
+        .iOS(.v12) // Adjusted to a minimum version that is more common for current SPM usage
+    ],
+    products: [
+        // Defines the library product that includes all the targets (subspecs in Podspec terms)
+        .library(
+            name: "CLImageEditor",
+            targets: [
+                "CLImageEditorCore"
+                     ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "CLImageEditorCore",
+            path: "CLImageEditor",
+//            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("CLImageEditor")
+            ]
+        )
+    ]
+)
